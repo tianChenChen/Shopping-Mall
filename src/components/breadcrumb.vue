437821f52e8a20eb3a93b56a7f782d91be4947f5
celia <tcc_2007@126.com>
2018-08-03 16:37:10 +0800

    新增内容

---
+++ src/components/breadcrumb.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="breadcrumb">
+    <ul>
+      <li><a href="javascript:;">首页</a>/</li>
+      <li><slot name="bread">产品</slot></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import 'assets/css/layout.css'
+
+export default {
+  
+}
+</script>
+
+
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <breadcrumb slot="bread"></breadcrumb>
+    <div class="main">
+      <div class="main_left">
+        <ul>
+          <p>分类</p>
+          <li>产品1</li>
+          <li>产品2</li>
+        </ul>
+      </div>
+      <div class="main_rig">
+        <ul>
+          <li>
+            <div class="main_rig_pro">
+              <div class="img">
+                <img src="@/assets/img/product-01.jpg"/>
+              </div>
+              <div class="main_rig_title">
+                <h6>desingse chair</h6>
+                <p>$799</p>
+                <a href="javascript:;">discover</a>
+                <a href="javascript:;">ADD TO CART</a>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'assets/css/base.css'
+import 'assets/css/goodlist.css'
+import Breadcrumb from 'components/breadcrumb.vue'
+
+export default {
+  components: {
+    Breadcrumb
+  }
+}
+</script>
+
+<style lang="css" scoped>
+/* @import 'assets/css/base.css';
+@import 'assets/css/goodlist.css'; */
+</style>
+
+
